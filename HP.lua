48815fd01dc43d2de5f491a8cf02a5ea6dcfee88
andreySy4 <andreysy4@gmail.com>
2020-02-07 14:14:24 +0300

    Добавлено читение из файла начального и изменённого кол. HP

--- HP.lua
+++ HP.lua
@@ -1,10 +1,12 @@
--- Script created for GameGuardian 89.1
--- Lua help: http://gameguardian.net/help/
-
--- options
+---------------------------------------------------
+----- Script created for GameGuardian 89.1 --------
+----- Lua help: http://gameguardian.net/help/ -----
+---------------------------------------------------
 local scriptName = [=====[HP freezer]=====]
-local scriptVersion = '0.0.0'
+local scriptVersion = '0.0.1'
 local scriptAuthor = 'Andrey'
+gg.require('89.0')
+gg.toast(scriptName..' v'..scriptVersion..' by '..scriptAuthor)
 
 function inputHP(massage)
 	local resTable
@@ -20,45 +22,65 @@ function sleepScript()
 	end
 end
 
+function readFromFile()
+	file = io.open("Saves.txt", "r")
+	first = file:read "*l"
+	second = file:read "*l"
+	file:close()
+	return first, second
+end
 
+function saveToFile(startHP, newHP)
 
--- init
-gg.require('89.0')
-gg.toast(scriptName..' v'..scriptVersion..' by '..scriptAuthor)
+end
 
--- main code
+---------------------------------------------------
+---------------------   Main   --------------------
+---------------------------------------------------
 local startHP = 0
 local newHP = 0
 
---while 1 do
+local status, startHP_S, newHP_S = pcall(readFromFile)
+if status then
+	startHP = tonumber(startHP_S) and tonumber(startHP_S) or 0
+	newHP = tonumber(newHP_S) and tonumber(newHP_S) or 0
+else
+	startHP = 0
+	newHP = 0
+end	
+
+gg.clearResults()
+gg.clearList()	
+
+gg.processPause()
+startHP = inputHP('Начальное количество HP:')
+gg.searchNumber(startHP, gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.processResume()
+gg.setVisible(false)
+gg.toast('Измените кол. HP, а затем откройте GG')
+
+while true do
 	sleepScript()
 	gg.processPause()
-	startHP = inputHP('Начальное количество HP:')
-	gg.searchNumber(startHP, gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+	newHP = inputHP('Изменнённое значение HP:')
+	gg.searchNumber(newHP, gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+	if gg.getResultCount() < 5 then
+		break
+	end
 	gg.processResume()
 	gg.setVisible(false)
-	gg.toast('Измените кол. HP, а затем откройте GG')
+	gg.toast('Результатов слишком много. Ещё немного измените HP')
+end
 
-	while true do
-		sleepScript()
-		gg.processPause()
-		newHP = inputHP('Изменнённое значение HP:')
-		gg.searchNumber(newHP, gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-		if gg.getResultCount() < 5 then
-			break
-		end
-		gg.processResume()
-		gg.setVisible(false)
-		gg.toast('Результатов слишком много. Ещё немного измените HP')
-	end
-	t = gg.getResults(5, nil, nil, nil, nil, nil, nil, nil, nil)
-	gg.editAll(startHP, gg.TYPE_DWORD)
-	t[1].freeze = true
-	--gg.clearResults()
-	gg.setVisible(false)
-	gg.toast('Бесконечное здоровье успешно активировано')
---end
-
+local resTable = gg.getResults(5, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(resTable) do --gg.getResultCount()
+	resTable[i].value = startHP
+	resTable[i].freeze = true
+end
+gg.addListItems(resTable)
+gg.processResume()
+gg.setVisible(false)
+gg.toast('Бесконечное здоровье успешно активировано')
 
 
 
